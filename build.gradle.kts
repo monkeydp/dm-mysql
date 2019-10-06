23063799ef3d7271f7d38be82dbbe80f5a475102
xuchen <monkeydpotato@gmail.com>
2019-10-07 05:40:18 +0800

    all projects use io.spring.dependency-management

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    // spring (only use for dependency management)
+    id("org.springframework.boot") version "2.1.9.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    // kotlin
     kotlin("jvm") version "1.2.71"
 }
 
@@ -19,7 +23,7 @@ dependencies {
     // monkeydp
     implementation("com.monkeydp.common:tools")
     // junit
-    testImplementation("junit:junit:4.12")
+    testImplementation("junit:junit")
 }
 
 tasks.withType<KotlinCompile> {
