c6cbbf48c75c613ce7b62c1b8eff89f85c5e4b9d
xuchen <monkeydpotato@gmail.com>
2019-10-08 10:12:16 +0800

    *

--- src/main/kotlin/com/monkeydp/daios/dm/mysql/connection/MysqlConnectionFactory.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/mysql/connection/MysqlConnectionFactory.kt
@@ -1,8 +1,15 @@
 package com.monkeydp.daios.dm.mysql.connection
 
+import com.monkeydp.daios.dm.base.connection.AbstractConnectionFactory
+import com.monkeydp.daios.dms.sdk.connection.Connection
+import com.monkeydp.daios.dms.sdk.connection.ConnectionProfile
+
 /**
  * @author iPotato
  * @date 2019/10/7
  */
-class MysqlConnectionFactory {
+class MysqlConnectionFactory : AbstractConnectionFactory() {
+    override fun newConnection(profile: ConnectionProfile): Connection {
+        return Connection(5L, 1L)
+    }
 }
