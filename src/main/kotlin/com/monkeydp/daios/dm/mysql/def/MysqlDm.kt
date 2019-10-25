e2a341e6b885c8a00508ba97180960ac6b2d520b
iPotato <monkeydpotato@gmail.com>
2019-10-25 22:50:50 +0800

    temp

--- src/main/kotlin/com/monkeydp/daios/dm/mysql/def/MysqlDm.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/mysql/def/MysqlDm.kt
@@ -1,6 +1,8 @@
 package com.monkeydp.daios.dm.mysql.def
 
 import com.monkeydp.daios.dm.mysql.connection.MysqlConnectionFactory
+import com.monkeydp.daios.dm.mysql.metadata.instruction.MysqlActionType
+import com.monkeydp.daios.dm.mysql.metadata.instruction.MysqlTargetType
 import com.monkeydp.daios.dms.sdk.datasource.Datasource
 import com.monkeydp.daios.dms.sdk.datasource.Datasource.DsVersion.MYSQL_5_7
 import com.monkeydp.daios.dms.sdk.datasource.Datasource.DsVersion.MYSQL_8_0
@@ -12,15 +14,21 @@ import com.monkeydp.daios.dms.sdk.dm.Dm.*
  * @date 2019/10/8
  */
 class MysqlDm : Dm {
-
+    
     override val datasource = Datasource.MYSQL
     override val dsDefs = listOf(Mysql57, Mysql80)
-    override val implClassNames = MysqlImplClassNames
-
-    object MysqlImplClassNames : ImplClassNames {
-        override val connectionFactory = MysqlConnectionFactory::class.qualifiedName
+    override val implClassnames = MysqlImplClassnames
+    override val implEnumClassnames = MysqlImplEnumClassnames
+    
+    object MysqlImplClassnames : ImplClassnames {
+        override val connectionFactory = MysqlConnectionFactory::class.qualifiedName!!
     }
-
+    
+    object MysqlImplEnumClassnames : ImplEnumClassnames {
+        override val actionType = MysqlActionType::class.qualifiedName!!
+        override val targetType = MysqlTargetType::class.qualifiedName!!
+    }
+    
     private object Mysql57 : DsDef {
         override val version = MYSQL_5_7
         override val driver = DsDriver(
@@ -28,7 +36,7 @@ class MysqlDm : Dm {
                 classname = "com.mysql.jdbc.Driver"
         )
     }
-
+    
     private object Mysql80 : DsDef {
         override val version = MYSQL_8_0
         override val driver = DsDriver(
