63fac114cd589e072660deacad2c50482414d341
iPotato <monkeydpotato@gmail.com>
2019-12-17 01:52:32 +0800

    add @KodeinBindArgs

--- src/main/kotlin/com/monkeydp/daios/dm/mysql/MysqlApp.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/mysql/MysqlApp.kt
@@ -1,7 +1,7 @@
 package com.monkeydp.daios.dm.mysql
 
 import com.monkeydp.daios.dm.base.AbstractDmApp
-import com.monkeydp.daios.dm.mysql.config.MysqlKodeinComponentConfig
+import com.monkeydp.daios.dm.mysql.config.MysqlKodeinCompConfig
 import com.monkeydp.daios.dm.mysql.config.kodein
 import com.monkeydp.daios.dms.sdk.annot.SdkDmApp
 import com.monkeydp.daios.dms.sdk.datasource.Datasource.MYSQL
@@ -17,7 +17,7 @@ import org.kodein.di.Kodein
 @SdkDmApp(MYSQL)
 object MysqlApp : AbstractDmApp() {
     override fun initDmKodein(): Kodein {
-        kodein.baseKodein = KodeinHelper.initKodein(MysqlKodeinComponentConfig, *kodeinModules)
+        kodein.baseKodein = KodeinHelper.initKodein(MysqlKodeinCompConfig, *kodeinModules)
         return kodein
     }
 }
--- src/main/kotlin/com/monkeydp/daios/dm/mysql/instruction/MysqlAction.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/mysql/instruction/MysqlAction.kt
@@ -4,6 +4,10 @@ import com.monkeydp.daios.dms.sdk.annot.SdkEnum
 import com.monkeydp.daios.dms.sdk.instruction.action.Action
 import com.monkeydp.daios.dms.sdk.instruction.action.GlobalAction
 import com.monkeydp.tools.enumx.EnumxOption
+import com.monkeydp.tools.ext.kodein.component.AbstractKodeinBindArgs
+import com.monkeydp.tools.ext.kodein.component.KodeinCompOption
+import org.kodein.di.generic
+import kotlin.reflect.KClass
 
 
 /**
@@ -12,6 +16,13 @@ import com.monkeydp.tools.enumx.EnumxOption
  */
 @SdkEnum
 @EnumxOption(GlobalAction::class)
+@KodeinCompOption(MysqlAction.KodeinBindArgs::class)
 enum class MysqlAction : Action<MysqlAction> {
-    MYSQL_EXAMPLE_ACTION
+    
+    MYSQL_EXAMPLE_ACTION;
+    
+    object KodeinBindArgs : AbstractKodeinBindArgs() {
+        override val typeToken = generic<KClass<out Action<*>>>()
+        override val overrides: Boolean? = true
+    }
 }
