9a0bbf1dc8ae3d2b44a9cde239afa3c913a91b18
iPotato <monkeydpotato@gmail.com>
2019-12-23 01:13:45 +0800

    Replace DmKodeinHelper with DmKodeinRepo

--- src/main/kotlin/com/monkeydp/daios/dm/mysql/config/MysqlKodein.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/mysql/config/MysqlKodein.kt
@@ -1,15 +1,15 @@
 package com.monkeydp.daios.dm.mysql.config
 
+import com.monkeydp.daios.dms.sdk.share.kodein.DmKodein
 import com.monkeydp.daios.dms.sdk.share.kodein.kodeinModules
 import com.monkeydp.tools.ext.kodein.KodeinHelper.bindComps
-import org.kodein.di.Kodein
 
 /**
  * @author iPotato
  * @date 2019/12/4
  */
 internal val kodein =
-        Kodein(true) {
+        DmKodein(true) {
             importAll(*kodeinModules)
-            bindComps(MysqlKodeinRepo.comps)
+            bindComps(MysqlKodeinCompRepo.comps)
         }
