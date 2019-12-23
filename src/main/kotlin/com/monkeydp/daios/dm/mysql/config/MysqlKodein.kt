8ca0336ad828c1b41de8d22f20e97424202c2757
iPotato <monkeydpotato@gmail.com>
2019-12-23 21:00:55 +0800

    *

--- src/main/kotlin/com/monkeydp/daios/dm/mysql/config/MysqlKodein.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/mysql/config/MysqlKodein.kt
@@ -2,17 +2,9 @@ package com.monkeydp.daios.dm.mysql.config
 
 import com.monkeydp.daios.dm.base.config.DmKodeinCompRepo
 import com.monkeydp.daios.dms.sdk.share.kodein.DmKodein
-import com.monkeydp.daios.dms.sdk.share.kodein.kodeinModules
-import com.monkeydp.tools.ext.kodein.KodeinHelper.bindComps
 
 /**
  * @author iPotato
  * @date 2019/12/4
  */
-internal val kodein =
-        DmKodein(true) {
-            importAll(*kodeinModules)
-            bindComps(MysqlKodeinCompRepo.comps)
-        }
-
-private object MysqlKodeinCompRepo : DmKodeinCompRepo()
+internal val kodein = DmKodein(object : DmKodeinCompRepo() {})
