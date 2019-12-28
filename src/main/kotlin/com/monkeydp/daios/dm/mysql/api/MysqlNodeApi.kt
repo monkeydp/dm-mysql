9f3f329288932c09b7d363fd3a8499ff586ed36a
iPotato <monkeydpotato@gmail.com>
2019-12-28 16:20:20 +0800

    *

--- src/main/kotlin/com/monkeydp/daios/dm/mysql/api/MysqlNodeApi.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/mysql/api/MysqlNodeApi.kt
@@ -1,8 +1,8 @@
 package com.monkeydp.daios.dm.mysql.api
 
 import com.monkeydp.daios.dm.base.api.AbstractNodeApi
-import com.monkeydp.daios.dm.base.jdbc.api.node.JdbcDbsLoader
-import com.monkeydp.daios.dm.base.jdbc.api.node.JdbcTablesLoader
+import com.monkeydp.daios.dm.base.jdbc.api.node.JdbcDbLoader
+import com.monkeydp.daios.dm.base.jdbc.api.node.JdbcTableLoader
 import com.monkeydp.daios.dm.base.metadata.node.def.UnhandledNodeDefException
 import com.monkeydp.daios.dm.base.metadata.node.def.sub.DbNd
 import com.monkeydp.daios.dm.base.metadata.node.def.sub.TableNd
@@ -36,10 +36,10 @@ object MysqlNodeApi : AbstractNodeApi() {
     private fun loadNodes(path: MysqlNodePath, def: NodeDef): List<Node> =
             (connContext.conn.rawConn as Connection).let {
                 when (def) {
-                    is DbNd -> JdbcDbsLoader.loadDbs(it, def, SHOW_DBS)
+                    is DbNd -> JdbcDbLoader.loadDbNodes(it, def, SHOW_DBS)
                     is TableNd -> {
                         useDb(it, path.dbName)
-                        JdbcTablesLoader.loadTables(it, def, SHOW_TABLES)
+                        JdbcTableLoader.loadTableNodes(it, def, SHOW_TABLES)
                     }
                     is TablesNd -> listOf(ndStruct.find<TablesNd>().create())
                     is ViewsNd -> listOf(ndStruct.find<ViewsNd>().create())
